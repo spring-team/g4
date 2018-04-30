fd6c52f0ef35747415511fe375ce9e777145219e
Rod Johnson <johnsonroda@gmail.com>
2018-04-30 16:14:23 +1000

    Update G4Application.java

--- src/main/java/com/atomist/G4Application.java
+++ src/main/java/com/atomist/G4Application.java
@@ -3,8 +3,6 @@ package com.atomist;
 
 import java.io.File;
 
-import java.util.*;
-
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
